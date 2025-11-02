c10ef24d6b45545f320fc3c233785a7292fa5970
Shahed Iqbal Chowdhury <shahed.chowdhury@uni-potsdam.de>
2025-11-02 15:34:51 +0100

    fetch data using sql

---
+++ SQL/fact_engagement_data.sql
@@ -0,0 +1,17 @@
+-- Query to clean and normalize the engagement_data table
+
+SELECT 
+    EngagementID,  
+    ContentID, 
+	CampaignID,  
+    ProductID,  
+    UPPER(REPLACE(ContentType, 'Socialmedia', 'Social Media')) AS ContentType,  
+    LEFT(ViewsClicksCombined, CHARINDEX('-', ViewsClicksCombined) - 1) AS Views,  -- Extracts the Views part from the ViewsClicksCombined column by taking the substring before the '-' character
+    RIGHT(ViewsClicksCombined, LEN(ViewsClicksCombined) - CHARINDEX('-', ViewsClicksCombined)) AS Clicks,  -- Extracts the Clicks part from the ViewsClicksCombined column by taking the substring after the '-' character
+    Likes,  -- Selects the number of likes the content received
+    -- Converts the EngagementDate to the dd.mm.yyyy format
+    FORMAT(CONVERT(DATE, EngagementDate), 'dd.MM.yyyy') AS EngagementDate  
+FROM 
+    dbo.engagement_data 
+WHERE 
+    ContentType != 'Newsletter';  -- Filters out rows where ContentType is 'Newsletter' 
